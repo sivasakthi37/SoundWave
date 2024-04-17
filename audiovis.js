42c0f98514bd9190eee6e28d751d411aee59f664
qburst-sivasakthi <sivasakthi@qburst.com>
2024-04-17 21:54:29 +0530

    final wave code

--- audiovis.js
+++ audiovis.js
@@ -1,4 +1,3 @@
-//GLOBALS
 var ctx;
 var width, height;
 
@@ -7,58 +6,99 @@ var analyser;
 
 var run = true;
 
-var hue = 0;
-var hueAdd = 1;
+// var hue = 0;
+// var hueAdd = 1;
+let audio
+
 
 window.addEventListener("load", (event) => {
     let theCanvas = document.getElementById("aCanvas");
     resizeCanvas(theCanvas, false);
-    width = theCanvas.width;
-    height = theCanvas.height;
+    theCanvas.width = 400
+    theCanvas.height = 400
+    width = 400;
+    height = 400;
 
     ctx = theCanvas.getContext("2d");
-
-    // var radioValue = document.querySelectorAll('input[name="vis"]').forEach(rad => {
-    //     rad.addEventListener("click", function (event) {
-    //         changeVis();
-    //     });
-    // });
-
-    // let startButton = document.getElementById("start");
-    console.log('startButton startButton');
-    // startButton.addEventListener("click", startVis);
-
-
-    const handleMessage = (event) => {
-        console.log("event", event);
+    // console.log('envvvvvvv', import.meta.env.VITE_GOOGLE_API)
+    const handleMessage = async (event) => {
+        event.preventDefault();
+        // console.log("event", event);
         if (typeof event.data === 'string') {
             const message = event.data && JSON.parse(event.data);
             console.log("Received message:", message);
+            if (message.action === "stop") {
 
-            if (message.action === "init") {
-                console.log('init call');
-                startVis()
+                if (audio) {
+                    audio.pause();  // Pauses the playback
+                    audio.currentTime = 0;  // Resets the playback position to the start
+                }
             }
+            if (message.text) {
+                const requestOptions = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        input: {
+                            text: message.text
+                        },
+                        voice: {
+                            languageCode: "en-US",
+                            name: "en-US-Studio-O"
+                        },
+                        audioConfig: {
+                            audioEncoding: "MP3",
+                            effectsProfileId: ["small-bluetooth-speaker-class-device"],
+                            pitch: 0,
+                            speakingRate: 1
+                        }
+                    }),
+                };
+                const apiKey = import.meta.env.VITE_GOOGLE_API;
+                await fetch(`https://texttospeech.googleapis.com/v1/text:synthesize?key=${apiKey}`, requestOptions)
+                    .then(response => response.json())
+                    .then(responseData => {
+                        const audioContent = responseData.audioContent;
+                        generateAudio(audioContent)
+                    })
+                    .catch(error => {
+                        console.error("Error:", error);
+                    });
+            }
+
         }
     };
 
-
-
     window.addEventListener('message', handleMessage)
-
-
 });
 
-function startVis() {
+async function generateAudio(base64) {
+    const byteCharacters = atob(base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    console.log("Bytes Array", byteArray)
+
+    const blob = new Blob([byteArray], { type: "audio/mpeg" });
+    const url = URL.createObjectURL(blob);
+
+    audio = document.getElementById("audioSource");
+    audio.src = url;
+    audio.load();
+    audio.play();
+    startVis(audio);
+}
+
+function startVis(audioElement) {
     // shape style
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
     ctx.fillRect(0, 0, width, height);
     ctx.lineWidth = 3;
     ctx.strokeStyle = '#16D1FE';
-
-    // request frame
-    let audioElement = document.getElementById("audioSource");
-    audioElement.play()
 
     // make sure AudioContext will work fine in different browsers
     let audioCtx = new AudioContext();
@@ -83,131 +123,6 @@ function startVis() {
     audioVisualize_3();
 }
 
-// function changeVis() {
-//     var radioValue = document.querySelector('input[name="vis"]:checked').value;
-
-//     run = false;
-
-//     if (radioValue == "vis1") {
-//         setTimeout(function () {
-//             analyser.fftSize = 512;
-//             let bufferLength = analyser.frequencyBinCount;
-//             dataArray = new Uint8Array(bufferLength);
-
-//             ctx.fillStyle = 'rgb(0, 0, 0)';
-//             ctx.fillRect(0, 0, width, height);
-//             ctx.lineWidth = 3;
-//             ctx.strokeStyle = 'orange';
-//             run = true;
-//             audioVisualize_1();
-//         }, 200);
-//     }
-//     else if (radioValue == "vis2") {
-//         setTimeout(function () {
-//             analyser.fftSize = 256;
-//             let bufferLength = analyser.frequencyBinCount;
-//             dataArray = new Uint8Array(bufferLength);
-
-//             ctx.fillStyle = 'rgb(0, 0, 0)';
-//             ctx.fillRect(0, 0, width, height);
-//             ctx.lineWidth = 3;
-//             ctx.strokeStyle = 'orange';
-//             run = true;
-//             audioVisualize_2();
-//         }, 200);
-//     }
-//     else if (radioValue == "vis3") {
-//         setTimeout(function () {
-//             analyser.fftSize = 256;
-//             let bufferLength = analyser.frequencyBinCount;
-//             dataArray = new Uint8Array(bufferLength);
-
-//             ctx.fillStyle = 'rgb(0, 0, 0)';
-//             ctx.fillRect(0, 0, width, height);
-//             ctx.lineWidth = 3;
-//             ctx.strokeStyle = 'orange';
-//             run = true;
-//             audioVisualize_3();
-//         }, 200);
-//     }
-//     else if (radioValue == "vis4") {
-//         setTimeout(function () {
-//             analyser.fftSize = 64;
-//             let bufferLength = analyser.frequencyBinCount;
-//             dataArray = new Uint8Array(bufferLength);
-
-//             ctx.lineWidth = width / 32;
-//             run = true;
-//             audioVisualize_4();
-//         }, 200);
-//     }
-// }
-
-// //Line Graph Visualization
-// function audioVisualize_1() {
-//     if (!run) {
-//         return;
-//     }
-//     analyser.getByteTimeDomainData(dataArray);
-
-//     // clear the previous shape
-//     ctx.fillRect(0, 0, width, height);
-//     ctx.beginPath();
-//     let sliceWidth = width * (1.0 / dataArray.length);
-//     let x = 0;
-//     let v = dataArray[0] / 128.0;
-//     let y = v * height / 2;
-//     ctx.moveTo(x, y);
-//     for (let i = 1; i < dataArray.length; i++) {
-//         v = dataArray[i] / 128.0;
-//         y = v * height / 2;
-//         ctx.lineTo(x, y);
-//         x += sliceWidth;
-//     }
-//     ctx.lineTo(width, height / 2);
-//     ctx.stroke();
-
-//     requestAnimationFrame(audioVisualize_1);
-// }
-
-// //Circle Visualization
-// function audioVisualize_2() {
-//     if (!run) {
-//         return;
-//     }
-//     analyser.getByteTimeDomainData(dataArray);
-
-//     // clear the previous shape
-//     ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
-//     ctx.beginPath();
-//     ctx.rect(0, 0, width, height);
-//     ctx.fill();
-
-//     let cX = width / 2;
-//     let cY = height / 2;
-
-//     let radianAdd = Constants.TWO_PI / dataArray.length;
-//     let radian = 0;
-//     ctx.fillStyle = "hsl(" + hue + ", 100%, 50%)";
-//     for (let i = 0; i < dataArray.length; i++) {
-//         v = dataArray[i];
-
-//         let x = v * Math.cos(radian) + cX;
-//         let y = v * Math.sin(radian) + cY;
-
-//         ctx.beginPath();
-//         ctx.arc(x, y, 5, 0, Constants.TWO_PI, false);
-//         ctx.fill();
-
-//         radian += radianAdd;
-//     }
-//     hue += hueAdd;
-//     if (hue > 360) {
-//         hue = 0;
-//     }
-
-//     requestAnimationFrame(audioVisualize_2);
-// }
 
 //Circle Spikes Visualization
 function audioVisualize_3() {
@@ -229,7 +144,7 @@ function audioVisualize_3() {
 
     let radianAdd = Constants.TWO_PI / dataArray.length;
     let radian = 0;
-    console.log('radianAdd', radianAdd);
+    // console.log('radianAdd', radianAdd);
     for (let i = 0; i < dataArray.length; i++) {
         // console.log('radian');
         let x = radius * Math.cos(radian) + cX;
@@ -237,7 +152,7 @@ function audioVisualize_3() {
         ctx.beginPath();
         ctx.moveTo(x, y);
 
-        v = dataArray[i];
+        var v = dataArray[i];
         if (v < radius) {
             v = radius;
         }
@@ -253,38 +168,6 @@ function audioVisualize_3() {
 
     requestAnimationFrame(audioVisualize_3);
 }
-
-// //Bar Graph Visualization
-// function audioVisualize_4() {
-//     if (!run) {
-//         return;
-//     }
-//     analyser.getByteTimeDomainData(dataArray);
-
-//     // clear the previous shape
-//     ctx.fillRect(0, 0, width, height);
-
-//     let sliceWidth = width / dataArray.length;
-//     let x = 0;
-//     let y = (height / 2) - 63.5;
-
-//     let h = 0;
-//     let iH = 360 / dataArray.length;
-//     for (let i = 0; i < dataArray.length; i++) {
-//         v = (height / 2) - (dataArray[i] / 2);
-
-//         ctx.strokeStyle = "hsl(" + h + ",100%,75%)";
-//         ctx.beginPath();
-//         ctx.moveTo(x, y)
-//         ctx.lineTo(x, v);
-//         ctx.stroke();
-
-//         x += sliceWidth;
-//         h += iH;
-//     }
-
-//     requestAnimationFrame(audioVisualize_4);
-// }
 
 
 function drawImageCentered() {
@@ -305,8 +188,4 @@ function drawImageCentered() {
     // Draw the image with the desired width and height
     ctx.drawImage(img, x + padding, y + padding, desiredWidth, desiredHeight);
 
-
-    // var x = (width - img.width) / 2;
-    // var y = (height - img.height) / 2;
-    // ctx.drawImage(img, x, y);
 }
